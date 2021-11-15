27a2c31b3c90e870b80286285f73426fc7dc9640
Brayan Puentes <brayansmithpuente@gmail.com>
2021-11-15 01:29:55 -0500

    first commit

---
+++ src/db/products.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE IF EXISTS productos;
+
+CREATE TABLE productos (
+  codigo int NOT NULL AUTO_INCREMENT,
+  nombre varchar(20) NOT NULL,
+  precio double NOT NULL,
+  inventario int NOT NULL,
+  PRIMARY KEY (codigo)
+);
---
+++ src/routes/routes.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const router = express.Router()
+
+const conexionMysql = require('../database')
+const query = require('./querys')
+
+router.get('/products', (req, res) => {
+    conexionMysql.query(query.select, (err,rows,fields) => {
+        if (!err) {
+            res.status(200).send(rows);
+        } else {
+            console.log(err)
+        }
+    })
+})
+router.get('/products/:id', (req, res) => {
+    let id = req.params.id
+    conexionMysql.query(query.selectByid, [id], (err,rows,fields) => {
+        if (!err) {
+            res.json(rows)
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+router.post('/products', (req, res) => {
+
+    let data = req.body
+    conexionMysql.query(query.save, [data], (err, rows, fields) => {
+        if (err) res.send(err)
+        else {
+            res.json("Product added");
+        }
+    })
+})
+
+router.put('/products/:id', (req, res) => {
+    let data = req.body
+    let id = req.params.id
+    conexionMysql.query(query.update, [data, id], (err, rows, fields) => {
+        if (err) res.send(err);
+        else {
+            res.json("Update complete")
+        }
+    })
+})
+router.delete('/products/:id', (req, res) => {
+
+    let data = req.params.id
+    conexionMysql.query(query.clear, [data], (err, rows, fields) => {
+        if (err) res.send(err)
+        else {
+            res.json("Product deleted");
+        }
+    })
+})
+
+
+module.exports = router 
